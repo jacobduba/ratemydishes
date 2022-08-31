d6607660a0c7868581479e8c9345378fe1951a11
Jacob Duba <jacob@mooshoe.me>
2022-08-30 21:23:28 -0500

    hello-world spring boot application

---
+++ Experiments/Jacob_Duba/hello-world/src/main/java/edu/iastate/jduba/coms309/HelloWorldApplication.java
@@ -0,0 +1,13 @@
+package edu.iastate.jduba.coms309;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+
+}

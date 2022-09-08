7f30cbff2c3cba52d4117a9c25723003cc86db93
kvk@iastate.edu <kvk@iastate.edu>
2022-09-08 11:36:42 -0500

    Fixed first Bean, but there is a second one

--- Experiments/Karthik_Kasarabada/hellopeople/src/main/java/coms309/people/Application.java
+++ Experiments/Karthik_Kasarabada/hellopeople/src/main/java/coms309/people/Application.java
@@ -2,14 +2,18 @@ package coms309.people;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Sample Spring Boot Application.
  * 
  * @author Vivek Bengre
  */
-
-@SpringBootApplication
+@EnableJpaRepositories(basePackages = {"coms309.people"})
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class Application {
 	
     public static void main(String[] args) throws Exception {

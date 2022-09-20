527bb3e701b7b30d791c3b0acb9983e8a3aaad03
Jacob Duba <jduba@iastate.edu>
2022-09-19 22:58:48 -0500

    (WIP) Add Restaurant JPAEntity

---
+++ Backend/src/test/java/com/example/backend/restaurant/RestaurantTest.java
@@ -0,0 +1,25 @@
+package com.example.backend.restaurant;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestaurantTest {
+
+    // TODO test initial restaurant
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+}

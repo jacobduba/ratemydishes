210dcb4a0a9eb93c7acf0e755019f511f492d2be
kvk@iastate.edu <kvk@iastate.edu>
2022-09-25 16:06:11 -0500

    Get request successfull, converted to JSON array. Will need to parse it and insert into database table.

--- Backend/src/main/java/GetLocations.java
+++ Backend/src/main/java/GetLocations.java
@@ -1,11 +1,16 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import java.io.*;
 import java.net.*;
 
 public class GetLocations {
 
-    public static String getHTML(String urlToRead) throws Exception {
+    public static JsonArray getHTML(String urlToRead) throws Exception {
         StringBuilder result = new StringBuilder();
-        URL url = new URL("https://dining.iastate.edu/wp-json/dining/menu-hours/get-locations/");
+        URL url = new URL(urlToRead);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         try (BufferedReader reader = new BufferedReader(
@@ -14,11 +19,21 @@ public class GetLocations {
                 result.append(line);
             }
         }
-        return result.toString();
+
+        JsonParser jsonParser = new JsonParser();
+        JsonArray jsonArray = (JsonArray) jsonParser.parse(result.toString());
+        return jsonArray;
+    }
+    public void populateTable(JsonArray arr) {  //Stopped here, need to parse JSON Array
+        for (int i = 0; i < arr.size(); i++) {
+            JsonObject jsonobj = arr.getAsJsonObject(i);
+            String name = String.valueOf(jsonobj.get("name"));
+            String url = jsonelement.getAsString("url");
+        }
     }
 
     public static void main(String[] args) throws Exception
     {
-        System.out.println(getHTML(args[0]));
+        System.out.println(getHTML("https://dining.iastate.edu/wp-json/dining/menu-hours/get-locations/"));
     }
 }

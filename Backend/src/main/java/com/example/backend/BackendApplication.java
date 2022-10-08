81987e5254811398de58bc8db5f107acb3272eed
kvk@iastate.edu <kvk@iastate.edu>
2022-10-08 16:07:14 -0500

    Updated branch to ensure merge reviews have been met.

--- Backend/src/main/java/com/example/backend/BackendApplication.java
+++ Backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,13 +1,7 @@
 package com.example.backend;
-
-import com.example.backend.location.GetLocations;
-import com.example.backend.location.LocationRepository;
-import com.example.backend.location.Locations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Service;
+
 
 
 @SpringBootApplication
@@ -17,8 +11,4 @@ public class BackendApplication {
         SpringApplication.run(BackendApplication.class, args);
     }
 
-   /* CommandLineRunner initLocation(LocationRepository lr) {
-        return args ->
-
-    }*/
 }

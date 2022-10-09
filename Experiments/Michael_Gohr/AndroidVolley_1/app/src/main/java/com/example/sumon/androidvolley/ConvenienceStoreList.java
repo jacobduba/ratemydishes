5d0ea1f5fb7f3850cdc9560a3c1bd039a3832cd7
Michael Gohr <mtgohr@iastate.edu>
2022-10-09 17:09:39 -0500

    final changes

---
+++ Experiments/Michael_Gohr/AndroidVolley_1/app/src/main/java/com/example/sumon/androidvolley/ConvenienceStoreList.java
@@ -0,0 +1,13 @@
+package com.example.sumon.androidvolley;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConvenienceStoreList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_convenience_store_list);
+    }
+}
---
+++ Experiments/Michael_Gohr/AndroidVolley_1/app/src/main/java/com/example/sumon/androidvolley/GetAndGoList.java
@@ -0,0 +1,13 @@
+package com.example.sumon.androidvolley;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GetAndGoList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_and_go_list);
+    }
+}

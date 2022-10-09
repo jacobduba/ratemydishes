04d568b252686d30ceb312be892f9665fad02b5c
kvk@iastate.edu <kvk@iastate.edu>
2022-10-09 17:13:57 -0500

    Got the Scheduled task to work

--- Backend/src/main/java/com/example/backend/BackendApplication.java
+++ Backend/src/main/java/com/example/backend/BackendApplication.java
@@ -6,17 +6,12 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-
+@Configuration
+@EnableScheduling
 @SpringBootApplication
 public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
-
-}
-@Configuration
-@EnableScheduling
-@ConditionalOnProperty(name = "scheduling.enabled", matchIfMissing = true)
-class enableConfiguration {
 
 }

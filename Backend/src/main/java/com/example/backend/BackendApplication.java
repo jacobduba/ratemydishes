ddf206d40e9f71b9110183d7440dfaad9bbd9db3
kvk@iastate.edu <kvk@iastate.edu>
2022-10-19 13:03:25 -0500

    Fixed json reading errors with Menus table. Should be working now.

--- Backend/src/main/java/com/example/backend/BackendApplication.java
+++ Backend/src/main/java/com/example/backend/BackendApplication.java
@@ -4,7 +4,6 @@ import com.example.backend.user.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -17,10 +16,10 @@ public class BackendApplication {
         SpringApplication.run(BackendApplication.class, args);
     }
 
-    /*@Bean
+    @Bean
     CommandLineRunner init(UserService us) {
        return args -> {
            us.createNewUser("admin", "admin");
         };
-    }*/
+    }
 }

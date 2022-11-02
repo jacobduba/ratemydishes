8f9a8730e8927c9a0030c2f5f04ae9b90078ebef
Brandon Zaragoza <brz0323@gmail.com>
2022-11-02 14:27:45 -0500

    Finally actually really fixed pipeline

--- Frontend/app/src/main/java/com/g1as6/ratemydishes/MenuList.java
+++ Frontend/app/src/main/java/com/g1as6/ratemydishes/MenuList.java
@@ -2,21 +2,16 @@ package com.g1as6.ratemydishes;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.constraintlayout.widget.ConstraintSet;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.g1as6.ratemydishes.app.AppController;
-import com.g1as6.ratemydishes.utils.MenuFragment;
 
 import org.json.JSONException;
 import org.json.JSONObject;

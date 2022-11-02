17b5220eea98b1ef19c64249d741e783a7d214fb
Brandon Zaragoza <brz0323@gmail.com>
2022-11-02 15:26:04 -0500

    Pager setup

---
+++ Frontend/app/src/main/java/com/g1as6/ratemydishes/fragment/DemoObjectFragment.java
@@ -0,0 +1,30 @@
+package com.g1as6.ratemydishes.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.g1as6.ratemydishes.R;
+
+// Instances of this class are fragments representing a single
+// object in our collection.
+public class DemoObjectFragment extends Fragment {
+    public static final String ARG_OBJECT = "object";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.menu_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+    }
+}

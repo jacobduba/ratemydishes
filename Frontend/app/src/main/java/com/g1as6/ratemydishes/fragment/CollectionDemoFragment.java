a69dc72b8178b2e7a60040b0f7435f8dd77d2e94
Brandon Zaragoza <brz0323@gmail.com>
2022-11-03 13:15:31 -0500

    Added a tab for every menu provided by restaurant

---
+++ Frontend/app/src/main/java/com/g1as6/ratemydishes/fragment/CollectionDemoFragment.java
@@ -0,0 +1,37 @@
+package com.g1as6.ratemydishes.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.g1as6.ratemydishes.R;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class CollectionDemoFragment extends Fragment {
+    // When requested, this adapter returns a DemoObjectFragment,
+    // representing an object in the collection.
+    DemoCollectionAdapter demoCollectionAdapter;
+    ViewPager2 viewPager;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        TabLayout tabLayout = container.findViewById(R.id.tab_layout);
+        new TabLayoutMediator(tabLayout, viewPager,(tab, position) -> tab.setText("OBJECT " + (position + 1))).attach();
+        return inflater.inflate(R.layout.menu_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        demoCollectionAdapter = new DemoCollectionAdapter(this);
+        viewPager = view.findViewById(R.id.pager);
+        viewPager.setAdapter(demoCollectionAdapter);
+    }
+}

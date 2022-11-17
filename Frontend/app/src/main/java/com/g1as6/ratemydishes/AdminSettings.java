2de34e7033247367019237e669104c73ad8e87fc
Michael Gohr <mtgohr@iastate.edu>
2022-11-17 12:53:17 -0600

    added buttons for admin screen

--- Frontend/app/src/main/java/com/g1as6/ratemydishes/AdminSettings.java
+++ Frontend/app/src/main/java/com/g1as6/ratemydishes/AdminSettings.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Switch;
+
 import com.g1as6.ratemydishes.app.AppVars;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +14,27 @@ import androidx.appcompat.app.AppCompatActivity;
 public class AdminSettings extends AppCompatActivity {
 
     ImageButton backToSettings;
-    Button switchWhirlyBirds;
+    Switch switchSeasons;
+    Switch switchUnion;
+    Switch switchConversations;
+    Switch switchFriley;
+    Switch switchClyde;
+    Switch switchHawthorn;
+    Switch switchMU;
+    Switch switchLance;
+    Switch switchDolce;
+    Switch switchHeaping;
+    Switch switchBookends;
+    Switch switchBusiness;
+    Switch switchRoast;
+    Switch switchCourt;
+    Switch switchDesign;
+    Switch switchWhirly;
+    Switch switchGentle;
+    Switch switchGlobal;
+    Switch switchMUMarket;
+    Switch switchAbe;
+    Switch switchGet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +42,28 @@ public class AdminSettings extends AppCompatActivity {
         setContentView(R.layout.activity_admin_settings);
 
         backToSettings = findViewById(R.id.adminToSettings);
+        switchSeasons = findViewById(R.id.switchSeasons);
+        switchUnion = findViewById(R.id.switchUnion);
+        switchConversations = findViewById(R.id.switchConversations);
+        switchFriley = findViewById(R.id.switchFriley);
+        switchClyde = findViewById(R.id.switchClyde);
+        switchHawthorn = findViewById(R.id.switchHawthorn);
+        switchMU = findViewById(R.id.switchMU);
+        switchLance = findViewById(R.id.switchLance);
+        switchDolce = findViewById(R.id.switchDolce);
+        switchHeaping = findViewById(R.id.switchHeaping);
+        switchBookends = findViewById(R.id.switchBookends);
+        switchBusiness = findViewById(R.id.switchBusiness);
+        switchRoast = findViewById(R.id.switchRoast);
+        switchCourt = findViewById(R.id.switchCourt);
+        switchDesign = findViewById(R.id.switchDesign);
+        switchWhirly = findViewById(R.id.switchWhirly);
+        switchGentle = findViewById(R.id.switchGentle);
+        switchGlobal = findViewById(R.id.switchGlobal);
+        switchMUMarket = findViewById(R.id.switchMUMarket);
+        switchAbe = findViewById(R.id.switchAbe);
+        switchGet = findViewById(R.id.switchGet);
+
 
         backToSettings.setOnClickListener(new View.OnClickListener() {
             @Override

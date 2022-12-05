6f3a05d18500546fa92d0f48425738bffeb92fb1
Michael Gohr <mtgohr@iastate.edu>
2022-12-05 16:07:31 -0600

    Trying out testing

---
+++ Frontend/app/src/main/java/com/g1as6/ratemydishes/UnitTest.java
@@ -0,0 +1,17 @@
+package com.g1as6.ratemydishes;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class UnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}

fccecf536dbcba943e3702f2b4fd8c314da63bfb
Jacob Duba <jduba@iastate.edu>
2022-12-06 19:35:01 -0600

    Four test cases

--- Backend/src/main/java/com/example/backend/user/payload/ChangePasswordRequest.java
+++ Backend/src/main/java/com/example/backend/user/payload/ChangePasswordRequest.java
@@ -1,6 +1,5 @@
 package com.example.backend.user.payload;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,10 +8,15 @@ import javax.validation.constraints.Size;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 public class ChangePasswordRequest extends AuthRequest {
     private String oldPassword;
     @Size(min = 5)
     private String newPassword;
+
+    public ChangePasswordRequest(String token, String oldPassword, String newPassword) {
+        super(token);
+        this.oldPassword = oldPassword;
+        this.newPassword = newPassword;
+    }
 }
--- Backend/src/test/java/com/example/backend/JacobTests.java
+++ Backend/src/test/java/com/example/backend/JacobTests.java
@@ -4,9 +4,12 @@ import com.example.backend.user.JwtTokenHelper;
 import com.example.backend.user.User;
 import com.example.backend.user.UserRepository;
 import com.example.backend.user.payload.AuthRequest;
+import com.example.backend.user.payload.ChangePasswordRequest;
 import com.example.backend.user.payload.LoginRequest;
+import com.example.backend.user.payload.RegisterRequest;
 import io.restassured.RestAssured;
 import org.json.JSONException;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,6 +19,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -38,6 +44,11 @@ class JacobTests {
         RestAssured.port = port;
     }
 
+    @AfterAll
+    public void afterAll() {
+        userRepository.deleteByNetId("user1");
+    }
+
     // Generate token for admin user, if user does not exist return invalid token
     public String getToken(String netId) {
         User user = userRepository.findByNetId(netId);
@@ -48,24 +59,52 @@ class JacobTests {
     }
 
     @ParameterizedTest
-    @CsvSource({"admin,admin,200", "admin,poop,401", "poop,admin,401" })
+    @CsvSource({"admin,admin,200", "admin,poop,401", "poop,admin,401"})
     public void login(String netId, String password, int status) throws JSONException {
         RestAssured.given()
-                .contentType("application/json")
-                .body(new LoginRequest(netId, password))
-                .post("/user/login")
-                .then()
-                .statusCode(status);
+                   .contentType("application/json")
+                   .body(new LoginRequest(netId, password))
+                   .post("/user/login")
+                   .then()
+                   .statusCode(status);
     }
 
     @ParameterizedTest
     @CsvSource({"200,admin", "401,invalid_user"})
     public void ping(int status, String netId) throws JSONException {
         RestAssured.given()
-                .contentType("application/json")
-                .body(new AuthRequest(getToken(netId)))
-                .post("/user/ping")
-                .then()
-                .statusCode(status);
+                   .contentType("application/json")
+                   .body(new AuthRequest(getToken(netId)))
+                   .post("/user/ping")
+                   .then()
+                   .statusCode(status);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"user1,aaaaa,200,true", "us,aaaaa,401,false", "user3,aaa,401,false", "user1,aaaaa,401,true"})
+    public void register(String netId, String password, int status, boolean shouldUserExists) throws JSONException {
+        RestAssured.given()
+                   .contentType("application/json")
+                   .body(new RegisterRequest(netId, password))
+                   .post("/user/register")
+                   .then()
+                   .statusCode(status);
+
+        User user = userRepository.findByNetId(netId);
+        if (shouldUserExists)
+            assertNotNull(user);
+        else
+            assertNull(user);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"user1,aaaaa,newpw,200", "user1,aaaaa,newpw,401", "user2,aaaaa,aaaaa,401"})
+    public void changePassword(String netId, String oldPassword, String newPassword, int status) {
+        RestAssured.given()
+                   .contentType("application/json")
+                   .body(new ChangePasswordRequest(getToken(netId), oldPassword, newPassword))
+                   .post("/user/changepw")
+                   .then()
+                   .statusCode(status);
     }
 }
